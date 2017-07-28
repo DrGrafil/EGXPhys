14a622fd2a258d72452ffa7330a7ad3f91283c7a
Metex <elliot_grafil@ufoh.com>
2017-07-28 13:21:49 -0700

    Astrophysics modifications

---
+++ EGXPhys/Astrophysics/Luminosity - Copy.hpp
@@ -0,0 +1,52 @@
+/// @file EGXPhys/Astrophysics/Luminosity.hpp
+///
+/// @brief Calculates luminosity of various types for stellar bodies
+///
+/// @author Elliot Grafil (Metex)
+/// @date 7/25/17
+/// @bug No known bugs.
+
+//=================================
+// Header guard
+#pragma once
+//=================================
+// Included dependencies
+#include <math.h>  
+
+#include "AstrophysicsConstants.hpp"
+
+//=================================
+// Forward declared dependencies
+
+//=================================
+//
+
+
+namespace EGXPhys
+{
+	/** 
+	*   @brief  Calculates the luminosity in Watts of a black body. 
+	*			\f[L=\sigma*A*T^4\f]   
+	*  
+	*   @param  area A(m^2) is the area of black body
+	*   @param  tempreture T(K) is tempreture of black body 
+	*   @return Luminosity L(W) for a black body
+	*/ 
+    template<typename T>
+    T blackbodyLuminosity(const T& area, const T& tempreture);
+    
+    /** 
+	*   @brief  Calculates the radio luminosity in W/Hz of a radio source. See https://en.wikipedia.org/wiki/Luminosity#Radio_luminosity.  
+	*  
+	*   @param  fluxDensity (W*m^2/Hz) is observed flux density 
+	*   @param  luminosityDistance (m) is the luminosity distance. See https://en.wikipedia.org/wiki/Luminosity_distance.
+	*   @param	redshift (dimensionless) for a black body. See https://en.wikipedia.org/wiki/Redshift.
+	*	@param	spectralIndex (dimensionless). The spectral index is typically -0.7.
+	*	@return Radio luminosity (W/Hz) of a radio source. 
+	*/ 
+    template<typename T>
+    T radioLuminosity(const T& fluxDensity, const T& luminosityDistance, const T& redshift, const T& spectralIndex);
+    
+} //namespace EGXPhys
+
+#include "header.inl"
