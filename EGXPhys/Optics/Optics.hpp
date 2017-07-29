f3c8ecfd571167413a08e03429d937f796f2a977
Metex <elliot_grafil@ufoh.com>
2017-07-28 20:41:19 -0700

    Added Optics field

---
+++ EGXPhys/Optics/Optics.hpp
@@ -0,0 +1,18 @@
+/// @file EGXPhys/Optics/Optics.hpp
+///
+/// @brief Includes all header files that are related to the field of Optics.
+///
+/// @author Elliot Grafil (Metex)
+/// @date 7/28/17
+/// @bug No known bugs.
+
+//=================================
+// Header guard
+#pragma once
+//=================================
+// Included dependencies
+
+//=================================
+#include "OpticsConstants.hpp"
+
+#include "Redshift.hpp"
