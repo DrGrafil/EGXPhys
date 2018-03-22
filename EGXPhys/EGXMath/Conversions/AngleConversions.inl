03e70af6d28baefb68738715ceaa97c49c8dae3d
Metex <elliot_grafil@ufoh.com>
2018-03-21 17:42:29 -0700

    Generalized Compass wind
    and tested it too

--- EGXPhys/EGXMath/Conversions/AngleConversions.inl
+++ EGXPhys/EGXMath/Conversions/AngleConversions.inl
@@ -15,7 +15,7 @@ namespace EGXMath {
 
 	template<typename T>
 	T DegreeToMilliradian(const T& decimalDegree) {
-		return decimalDegree * EGXMath::pi<T> / 180000.0;
+		return decimalDegree * EGXMath::pi<T> / 180.0 * 1000.0;
 	}
 
 	template<typename T>
@@ -31,33 +31,33 @@ namespace EGXMath {
 	//https://en.wikipedia.org/wiki/Minute_and_second_of_arc
 	template<typename T>
 	T DegreeToArcminute(const T& decimalDegree) {
-		return decimalDegree / 60.0;
+		return decimalDegree * 60.0;
 	}
 
 	//https://en.wikipedia.org/wiki/Minute_and_second_of_arc
 	template<typename T>
 	T DegreeToArcsecond(const T& decimalDegree) {
-		return decimalDegree / 3600.0;
+		return decimalDegree * 3600.0;
 	}
 
 	template<typename T>
 	T DegreeToMilliarcsecond(const T& decimalDegree) {
-		return decimalDegree / 3600000.0;
+		return decimalDegree * 3600000.0;
 	}
 
 	template<typename T>
 	T DegreeToMicroarcsecond(const T& decimalDegree) {
-		return decimalDegree / 3600000000.0;
+		return decimalDegree * 3600000000.0;
 	}
-//#include <functional>
+
 	template<typename T>
 	void DegreeToDegreesMinutesSeconds(const T& decimalDegree, T& degree, T& minute, T& second) {
-		/*
+
 		//See https://en.wikipedia.org/wiki/Decimal_degrees
 		degree = std::trunc(decimalDegree);
-		
-		minute = std::modulus( std::trunc( std::abs(decimalDegree) * 60.0), 60.0);
-		second = std::modulus( std::abs(decimalDegree) * 3600.0, 60.0);*/
+
+		minute = std::fmod( std::trunc( std::abs(decimalDegree) * 60.0), 60.0);
+		second = std::fmod( std::abs(decimalDegree) * 3600.0, 60.0);
 	}
 
 	template<typename T>
@@ -94,18 +94,22 @@ namespace EGXMath {
 	//https://en.wikipedia.org/wiki/Points_of_the_compass
 	template<typename T>
 	std::string DegreeToCompass8Wind(const T& decimalDegree, const bool abbreviate) {
-		return "not working";
+		return DegreeToCompassWind(decimalDegree, abbreviate, 8);
 	}
 	//https://en.wikipedia.org/wiki/Points_of_the_compass
 	template<typename T>
 	std::string DegreeToCompass16Wind(const T& decimalDegree, const bool abbreviate) {
-		return "not working";
+		return DegreeToCompassWind(decimalDegree, abbreviate, 16);
 	}
 	//https://en.wikipedia.org/wiki/Points_of_the_compass
 	template<typename T>
 	std::string DegreeToCompass32Wind(const T& decimalDegree, const bool abbreviate) {
-		static std::vector<std::string> pointName;
-		pointName = {
+		return DegreeToCompassWind(decimalDegree, abbreviate, 32);
+	}
+
+	template<typename T>
+	std::string DegreeToCompassWind(const T& decimalDegree, const bool abbreviate, const unsigned int winds) {
+		const static std::vector<std::string> pointName = {
 			"north",				"north by east",		"north-northeast",	"northeast by north",
 			"northeast",			"northeast by east",	"east-northeast",	"east by north",
 			"east",					"east by south",		"east-southeast",	"southeast by east",
@@ -116,9 +120,7 @@ namespace EGXMath {
 			"northwest",			"northwest by north",	"north-northwest",	"north by west"
 		};
 
-
-		static std::vector<std::string> pointAbreviation;
-		pointAbreviation = {
+		const static std::vector<std::string> pointAbreviation = {
 			"N",		"NbE",		"NNE",		"NEbN",
 			"NE",		"NEbE",		"ENE",		"EbN",
 			"E",		"EbS",		"ESE",		"SEbE",
@@ -129,10 +131,15 @@ namespace EGXMath {
 			"NW",		"NWbN",		"NNW",		"NbW"
 		};
 
-		T angle = decimalDegree;
-		angle += 5.625; // offset angle by half the distance of a wind. This means that North is now 0=11.25 instead of -5.625 to 5.625
+		
 
-						//Converts negative angle to positive
+		T windAngle = 360.0/(T)winds; // how much each slice of compass is in degrees. 11.25 in the case of 32 wind compass
+
+		T angle = decimalDegree;
+		angle += windAngle / 2.0; // offset angle by half the distance of a wind. 
+									//This means that North is now 0=11.25 instead of -5.625 to 5.625 in the case of a 32 wind compass
+
+		//Converts negative angle to positive
 		while (angle < 0.0) {
 			angle += 360.0;
 		}
@@ -141,14 +148,17 @@ namespace EGXMath {
 			angle -= 360.0;
 		}
 		//Angle now ranges from 0-360
+		unsigned int position = (unsigned int)(angle / windAngle);
+		
+		unsigned int positionScale = 32/winds; // for diffrent wind numbers the scale needs to be offset by a multiple number
+		//32 = 1, 16 = 2 8 = 4 ect...
 
-		unsigned int position = (unsigned int)angle / (unsigned int)32;
 
 		if (!abbreviate) {
-			return pointName[position];
+			return pointName[position*positionScale];
 		}
 		else {
-			return pointAbreviation[position];
+			return pointAbreviation[position*positionScale];
 		}
 	}
 
--- tests/EGXMath/Conversions/AngularConversionsTest.hpp
+++ tests/EGXMath/Conversions/AngularConversionsTest.hpp
@@ -4,95 +4,116 @@ TEST_CASE("Angular Conversions"){
 
 	//Degree
 
-	CHECK(DegreeToRadian(0.0) == Approx(0).scale(0.0).epsilon(0.001));
-	CHECK(DegreeToRadian(0) == Approx(0).scale(0.0).epsilon(0.001));
-
+	CHECK(DegreeToRadian(0.0) == 0.0);
 	CHECK(DegreeToRadian(90.0) == Approx(EGXMath::pi<double>/2.0).scale(0.0).epsilon(0.001));
 	CHECK(DegreeToRadian(180.0) == Approx(EGXMath::pi<double>).scale(0.0).epsilon(0.001));
 
 	
-	CHECK(DegreeToMilliradian(0) == Approx(0).scale(0.0).epsilon(0.001));
+	CHECK(DegreeToMilliradian(0.0) == 0.0);
 	CHECK(DegreeToMilliradian(90.0) == Approx(EGXMath::pi<double>*1000.0 / 2.0).scale(0.0).epsilon(0.001));
 	CHECK(DegreeToMilliradian(180.0) == Approx(EGXMath::pi<double>*1000.0).scale(0.0).epsilon(0.001));
 
 
-	CHECK(DegreeToArcminute(0) == Approx(0).scale(0.0).epsilon(0.001));
+	CHECK(DegreeToArcminute(0.0) == 0.0);
 	CHECK(DegreeToArcminute(90.0) == Approx(90.0*60.0).scale(0.0).epsilon(0.001));
 	CHECK(DegreeToArcminute(90) == Approx(90.0*60.0).scale(0.0).epsilon(0.001));
 
-	CHECK(DegreeToArcminute(0) == Approx(0).scale(0.0).epsilon(0.001));
-	CHECK(DegreeToArcminute(90.0) == Approx(90.0*60.0*60).scale(0.0).epsilon(0.001));
-	CHECK(DegreeToArcminute(90) == Approx(90.0*60.0*60).scale(0.0).epsilon(0.001));
+	CHECK(DegreeToArcsecond(0.0) == 0.0);
+	CHECK(DegreeToArcsecond(90.0) == Approx(90.0*60.0*60).scale(0.0).epsilon(0.001));
+	CHECK(DegreeToArcsecond(90) == Approx(90.0*60.0*60).scale(0.0).epsilon(0.001));
 
-	CHECK(DegreeToMilliarcsecond(0) == Approx(0).scale(0.0).epsilon(0.001));
+	CHECK(DegreeToMilliarcsecond(0.0) == 0.0);
 	CHECK(DegreeToMilliarcsecond(90.0) == Approx(90.0*60.0 * 60*1000).scale(0.0).epsilon(0.001));
 	CHECK(DegreeToMilliarcsecond(90) == Approx(90.0*60.0 * 60 * 1000).scale(0.0).epsilon(0.001));
 
-	CHECK(DegreeToMicroarcsecond(0) == Approx(0).scale(0.0).epsilon(0.001));
-	CHECK(DegreeToMicroarcsecond(90.0) == Approx(90.0*60.0 * 60 * 1000*1000).scale(0.0).epsilon(0.001));
-	CHECK(DegreeToMicroarcsecond(90) == Approx(90.0*60.0 * 60 * 1000*1000).scale(0.0).epsilon(0.001));
+	CHECK(DegreeToMicroarcsecond(0.0) == 0.0);
+	CHECK(DegreeToMicroarcsecond(90.0) == Approx(90.0*60.0 * 60.0 * 1000.0* 1000.0).scale(0.0).epsilon(0.001));
+	CHECK(DegreeToMicroarcsecond(180.0) == Approx(180.0*60.0 * 60.0 * 1000.0*1000.0).scale(0.0).epsilon(0.001));
 
-	float decdegree = 0.0f; float degree = 0.0f; float minute = 0.0f; float second = 0.0f;
+	double decdegree = 0.0f; double degree = 0.0f; double minute = 0.0f; double second = 0.0f;
 
 	DegreeToDegreesMinutesSeconds(decdegree, degree, minute, second);
-	CHECK(degree == Approx(0).scale(0.0).epsilon(0.001));
-	CHECK(minute == Approx(0).scale(0.0).epsilon(0.001));
-	CHECK(second == Approx(0).scale(0.0).epsilon(0.001));
+	CHECK(degree == 0.0);
+	CHECK(minute == 0.0);
+	CHECK(second == 0.0);
 
-	decdegree = 66.91827f;
+	decdegree = 66.91827;
 	DegreeToDegreesMinutesSeconds(decdegree, degree, minute, second);
 	CHECK(degree == Approx(66).scale(0.0).epsilon(0.001));
 	CHECK(minute == Approx(55).scale(0.0).epsilon(0.001));
 	CHECK(second == Approx(5.772).scale(0.0).epsilon(0.001));
 
-	decdegree = -145.35;
+	decdegree = -145.351;
 	DegreeToDegreesMinutesSeconds(decdegree, degree, minute, second);
 	CHECK(degree == Approx(-145).scale(0.0).epsilon(0.001));
 	CHECK(minute == Approx(21).scale(0.0).epsilon(0.001));
-	CHECK(second == Approx(0).scale(0.0).epsilon(0.001));
+	CHECK(second == Approx(3.6).scale(0.0).epsilon(0.001));
 
-	decdegree = -145.35;
+	decdegree = -145.351;
 	DegreeToDMS(decdegree, degree, minute, second);
 	CHECK(degree == Approx(-145).scale(0.0).epsilon(0.001));
 	CHECK(minute == Approx(21).scale(0.0).epsilon(0.001));
-	CHECK(second == Approx(0).scale(0.0).epsilon(0.001));
+	CHECK(second == Approx(3.6).scale(0.0).epsilon(0.001));
 
-	float hour = 0.0f;
+	double hour = 0.0;
 	decdegree = 0.0;
 	DegreeToHoursMinutesSeconds(decdegree, hour, minute, second);
-	CHECK(hour == Approx(0).scale(0.0).epsilon(0.001));
-	CHECK(minute == Approx(0).scale(0.0).epsilon(0.001));
-	CHECK(second == Approx(0).scale(0.0).epsilon(0.001));
+	CHECK(hour == 0.0);
+	CHECK(minute == 0.0);
+	CHECK(second == 0.0);
 
-	decdegree = 66.918277f;
+	decdegree = 66.918277;
 	DegreeToHoursMinutesSeconds(decdegree, hour, minute, second);
 	CHECK(hour == Approx(4).scale(0.0).epsilon(0.001));
 	CHECK(minute == Approx(27).scale(0.0).epsilon(0.001));
 	CHECK(second == Approx(40.3865).scale(0.0).epsilon(0.001));
 
-	decdegree = -58.2314f;
+	decdegree = -58.2314;
 	DegreeToHoursMinutesSeconds(decdegree, hour, minute, second);
 	CHECK(hour == Approx(-3).scale(0.0).epsilon(0.001));
 	CHECK(minute == Approx(52).scale(0.0).epsilon(0.001));
 	CHECK(second == Approx(55.536).scale(0.0).epsilon(0.001));
 
 
-	decdegree = -58.2314f;
+	decdegree = -58.2314;
 	DegreeToHMS(decdegree, hour, minute, second);
 	CHECK(hour == Approx(-3).scale(0.0).epsilon(0.001));
 	CHECK(minute == Approx(52).scale(0.0).epsilon(0.001));
 	CHECK(second == Approx(55.536).scale(0.0).epsilon(0.001));
 
 
-	CHECK(DegreeToGradian(0.0) == Approx(0.0).scale(0.0).epsilon(0.001));
+	CHECK(DegreeToGradian(0.0) == 0.0);
 	CHECK(DegreeToGradian(90.0) == Approx(100.0).scale(0.0).epsilon(0.001));
 	CHECK(DegreeToGradian(180.0) == Approx(200.0).scale(0.0).epsilon(0.001));
 
-	CHECK(DegreeToGon(0.0) == Approx(0.0).scale(0.0).epsilon(0.001));
+	CHECK(DegreeToGon(0.0) == 0.0);
 	CHECK(DegreeToGon(90.0) == Approx(100.0).scale(0.0).epsilon(0.001));
 	CHECK(DegreeToGon(180.0) == Approx(200.0).scale(0.0).epsilon(0.001));
 
 
 	
 	CHECK(DegreeToCompass32Wind(0.0, false) == "north");
+	CHECK(DegreeToCompass32Wind(90.0, false) == "east");
+	CHECK(DegreeToCompass32Wind(180.0, false) == "south");
+	CHECK(DegreeToCompass32Wind(270.0, false) == "west");
+
+	CHECK(DegreeToCompass32Wind(0.0, true) == "N");
+	CHECK(DegreeToCompass32Wind(90.0, true) == "E");
+	CHECK(DegreeToCompass32Wind(180.0, true) == "S");
+	CHECK(DegreeToCompass32Wind(270.0, true) == "W");
+
+	CHECK(DegreeToCompass32Wind(359.0, false) == "north");
+	CHECK(DegreeToCompass32Wind(361.0, false) == "north");
+	CHECK(DegreeToCompass32Wind(-1.0, false) == "north");
+
+	CHECK(DegreeToCompass16Wind(0.0, false) == "north");
+	CHECK(DegreeToCompass16Wind(90.0, false) == "east");
+	CHECK(DegreeToCompass16Wind(180.0, false) == "south");
+	CHECK(DegreeToCompass16Wind(270.0, false) == "west");
+
+	CHECK(DegreeToCompass8Wind(0.0, false) == "north");
+	CHECK(DegreeToCompass8Wind(90.0, false) == "east");
+	CHECK(DegreeToCompass8Wind(180.0, false) == "south");
+	CHECK(DegreeToCompass8Wind(270.0, false) == "west");
+
 }
