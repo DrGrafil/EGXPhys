47242e47db6a2c69c10cc73c44e05ebf77665479
Metex <elliot_grafil@ufoh.com>
2018-04-24 11:40:09 -0700

    Ellipse Linear Eccentricity implimented

--- EGXPhys/EGXMath/Geometry/2D/Ellipse/EllipseDirectrix.hpp
+++ EGXPhys/EGXMath/Geometry/2D/Ellipse/EllipseDirectrix.hpp
@@ -36,10 +36,10 @@ namespace EGXMath
 	*	@param semiMajorAxisInm \f$ a\ (m)\f$ Semi-major axis of the ellipse in meters.
 	*	@param semiMinorAxisInm \f$ b\ (m)\f$ Semi-minor axis of the ellipse in meters.
 	*   @return \f$ d\ (m)\f$ Directrix of the ellipse in meters.
+	*	@see EllipseSurfaceArea() for area of an ellipse.
 	*	@see EllipseCircumference() for circumference of an ellipse.
 	*	@see EllipseSemiMajorAxis() for semi-major axis of an ellipse.
 	*	@see EllipseSemiMinorAxis() for semi-minor axis of an ellipse.
-	*	@see EllipseDirectrix() for directrix of an ellipse.
 	*	@see EllipseFocalParameter() for focal parameter of an ellipse.
 	*	@see EllipseFocalLength() for focal length of an ellipse.
 	*	@see EllipseLinearEccentricity() for linear eccentricity of an ellipse.
@@ -58,10 +58,10 @@ namespace EGXMath
 	*	@param semiMajorAxisInm \f$ a\ (m)\f$ Semi-major axis of the ellipse in meters.
 	*	@param semiMinorAxisInm \f$ b\ (m)\f$ Semi-minor axis of the ellipse in meters.
 	*   @return \f$ d\ (m)\f$ Directrix of the ellipse in meters.
+	*	@see EllipseSurfaceArea() for area of an ellipse.
 	*	@see EllipseCircumference() for circumference of an ellipse.
 	*	@see EllipseSemiMajorAxis() for semi-major axis of an ellipse.
 	*	@see EllipseSemiMinorAxis() for semi-minor axis of an ellipse.
-	*	@see EllipseDirectrix() for directrix of an ellipse.
 	*	@see EllipseFocalParameter() for focal parameter of an ellipse.
 	*	@see EllipseFocalLength() for focal length of an ellipse.
 	*	@see EllipseLinearEccentricity() for linear eccentricity of an ellipse.
@@ -80,10 +80,10 @@ namespace EGXMath
 	*	@param semiMajorAxisInm \f$ a\ (m)\f$ Semi-major axis of the ellipse in meters.
 	*	@param linearEccentricityInm \f$ c\ (m)\f$ Linear eccentricity of the ellipse in meters.
 	*   @return \f$ d\ (m)\f$ Directrix of the ellipse in meters.
+	*	@see EllipseSurfaceArea() for area of an ellipse.
 	*	@see EllipseCircumference() for circumference of an ellipse.
 	*	@see EllipseSemiMajorAxis() for semi-major axis of an ellipse.
 	*	@see EllipseSemiMinorAxis() for semi-minor axis of an ellipse.
-	*	@see EllipseDirectrix() for directrix of an ellipse.
 	*	@see EllipseFocalParameter() for focal parameter of an ellipse.
 	*	@see EllipseFocalLength() for focal length of an ellipse.
 	*	@see EllipseLinearEccentricity() for linear eccentricity of an ellipse.
--- EGXPhys/EGXMath/Geometry/2D/Ellipse/EllipseLinearEccentricity.inl
+++ EGXPhys/EGXMath/Geometry/2D/Ellipse/EllipseLinearEccentricity.inl
@@ -1,6 +1,6 @@
-/// @file EGXMath/Geometry/2D/Ellipse/EllipseSurfaceArea.inl
+/// @file EGXMath/Geometry/2D/Ellipse/EllipseLinearEccentricity.inl
 ///
-/// @brief Implimentation of functions related to ellipse surface area.
+/// @brief Implimentation of functions related to ellipse linear eccentricity.
 /// @author Elliot Grafil (Metex)
 /// @date 4/23/18
 
@@ -8,9 +8,27 @@
 namespace EGXMath {
 
 	template <typename T>
-	T EllipseSurfaceArea(const T semiMajorAxisInm, const T semiMinorAxisInm)
-	{
-		return (T)EGXMath::pi * semiMajorAxisInm * semiMinorAxisInm;
+	T EllipseLinearEccentricity(const T semiMajorAxisInm, const T semiMinorAxisInm) {
+		EllipseLinearEccentricityFromSemiMajorAndSemiMinorAxes(semiMajorAxisInm, semiMinorAxisInm);
 	}
 
+
+	template <typename T>
+	T EllipseLinearEccentricityFromSemiMajorAndSemiMinorAxes(const T semiMajorAxisInm, const T semiMinorAxisInm) {
+		T a, b;
+		if (semiMajorAxisInm > semiMinorAxisInm) {
+			a = semiMajorAxisInm;
+			b = semiMinorAxisInm;
+		}
+		else {
+			b = semiMajorAxisInm;
+			a = semiMinorAxisInm;
+		}
+		return std::sqrt{ a*a - b * b };
+	}
+
+	template <typename T>
+	T EllipseLinearEccentricityFromEccentricity(const T semiMajorAxisInm, const T eccentricity) {
+		return semiMajorAxisInm * eccentricity;
+	}
 }//namespace EGXMath
